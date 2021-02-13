dc9c7ff70ee2e5ed17b70b323ba420c61c45fdb4
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2021-02-14 01:20:11 +0700

    Add android entry point into authentication activity, login fragment, and register fragment

--- app/src/main/java/com/healby/features/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/healby/features/authentication/AuthenticationActivity.kt
@@ -3,7 +3,9 @@ package com.healby.features.authentication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.healby.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/healby/features/authentication/LoginFragment.kt
+++ app/src/main/java/com/healby/features/authentication/LoginFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.healby.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
 
     override fun onCreateView(
