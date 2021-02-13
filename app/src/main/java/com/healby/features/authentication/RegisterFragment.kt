84b65fd111ade1210453c2fbcf50e787dcd94b7b
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2021-02-14 01:26:31 +0700

    Add binding in login and register fragment

--- app/src/main/java/com/healby/features/authentication/RegisterFragment.kt
+++ app/src/main/java/com/healby/features/authentication/RegisterFragment.kt
@@ -6,17 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.healby.R
+import com.healby.databinding.FragmentRegisterBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RegisterFragment : Fragment() {
 
+    private lateinit var binding: FragmentRegisterBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
