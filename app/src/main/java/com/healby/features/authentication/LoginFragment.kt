ee5e1edc3f5f8f943f30c964c868206c1f4929e9
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2021-02-14 02:11:58 +0700

    Add change password visibility function in login fragment

--- app/src/main/java/com/healby/features/authentication/LoginFragment.kt
+++ app/src/main/java/com/healby/features/authentication/LoginFragment.kt
@@ -1,19 +1,27 @@
 package com.healby.features.authentication
 
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
+import com.bumptech.glide.Glide
 import com.healby.R
 import com.healby.databinding.FragmentLoginBinding
+import com.healby.viewmodel.LoginFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
+    private val loginFragmentViewModel: LoginFragmentViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,5 +35,28 @@ class LoginFragment : Fragment() {
         binding.fragmentLoginRegisterButton.setOnClickListener {
             view.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
+        binding.fragmentLoginPassword.authenticationPasswordVisibilityImageview.setOnClickListener {
+            loginFragmentViewModel.setPasswordVisibility(!loginFragmentViewModel.getPasswordVisibility().value!!)
+        }
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        loginFragmentViewModel.getPasswordVisibility().observe(viewLifecycleOwner, Observer { visible ->
+            if(visible) {
+                Glide
+                    .with(binding.fragmentLoginPassword.authenticationPasswordVisibilityImageview.context)
+                    .load(ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_visibility_off_24))
+                    .into(binding.fragmentLoginPassword.authenticationPasswordVisibilityImageview)
+                binding.fragmentLoginPassword.authenticationPasswordEdittext.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            } else {
+                Glide
+                    .with(binding.fragmentLoginPassword.authenticationPasswordVisibilityImageview.context)
+                    .load(ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_visibility_24))
+                    .into(binding.fragmentLoginPassword.authenticationPasswordVisibilityImageview)
+                binding.fragmentLoginPassword.authenticationPasswordEdittext.transformationMethod = PasswordTransformationMethod.getInstance()
+            }
+            binding.fragmentLoginPassword.authenticationPasswordEdittext.setSelection(binding.fragmentLoginEmailEdittext.text.length)
+        })
     }
 }
--- app/src/main/java/com/healby/viewmodel/LoginFragmentViewModel.kt
+++ app/src/main/java/com/healby/viewmodel/LoginFragmentViewModel.kt
@@ -1,4 +1,21 @@
 package com.healby.viewmodel
 
-class LoginFragmentViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+// CHANGE VIEW MODEL EXTEND CLASS IF THIS CLASS HAS REQUEST TO THE NETWORK
+@HiltViewModel
+class LoginFragmentViewModel @Inject constructor(): ViewModel() {
+
+    private var passwordVisibility: MutableLiveData<Boolean> = MutableLiveData(false)
+
+    fun setPasswordVisibility(visibility: Boolean) {
+        passwordVisibility.postValue(visibility)
+    }
+
+    fun getPasswordVisibility(): LiveData<Boolean> = passwordVisibility
+
 }
